e5f4bb4404ee91f6d356522e6e1fa148e8cfa201
navi9000 <icdi7434@gmail.com>
2026-09-06 09:39:31 +0400

    add fonts

--- src/pages/login/api/loginAction.ts
+++ src/pages/login/api/loginAction.ts
@@ -13,16 +13,19 @@ export const loginAction = async ({ request }: { request: Request }) => {
     return { errors: z.treeifyError(validatedData.error).properties }
   }
 
-  const { accessToken } = await fetch("https://dummyjson.com/auth/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username,
-      password,
-      expiresInMins: 15,
-    }),
-    credentials: "include",
-  }).then((res) => res.json())
+  const { accessToken /*refreshToken*/ } = await fetch(
+    "https://dummyjson.com/auth/login",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+        expiresInMins: 15,
+      }),
+      credentials: "include",
+    },
+  ).then((res) => res.json())
 
   console.log({ accessToken })
 
